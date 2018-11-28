9ad4f6b4efd748923883c429c632648a6ad6d759
Neproify <yamasaki3214@gmail.com>
2018-11-28 23:34:01 +0100

    update laravel to 5.7

--- config/app.php
+++ config/app.php
@@ -166,7 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
-        DaveJamesMiller\Breadcrumbs\ServiceProvider::class,
+        DaveJamesMiller\Breadcrumbs\BreadcrumbsServiceProvider::class,
 
         //
 
@@ -227,7 +227,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Breadcrumbs' => DaveJamesMiller\Breadcrumbs\Facade::class,
+        'Breadcrumbs' => DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::class
 
     ],
 
--- resources/views/_partials/breadcrumbs.blade.php
+++ resources/views/_partials/breadcrumbs.blade.php
@@ -1,9 +1,15 @@
-<ol class="breadcrumb">
-    @foreach($breadcrumbs as $breadcrumb)
-        @if(!$breadcrumb->last)
-            <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
-        @else
-            <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
-        @endif
-    @endforeach
-</ol>
+@if (count($breadcrumbs))
+
+    <ol class="breadcrumb">
+        @foreach ($breadcrumbs as $breadcrumb)
+
+            @if ($breadcrumb->url && !$loop->last)
+                <li class="breadcrumb-item"><a href="{{ $breadcrumb->url }}">{{ $breadcrumb->title }}</a></li>
+            @else
+                <li class="breadcrumb-item active">{{ $breadcrumb->title }}</li>
+            @endif
+
+        @endforeach
+    </ol>
+
+@endif
